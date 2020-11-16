cc7e2239e4016b299642f4e58e4b1f646c67e2a9
Alex Shabanov <smokushka@gmail.com>
2020-11-16 17:08:47 -0500

    Arrays

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,17 @@
-function add(num1, num2) {
-    return num1 + num2;
+// function add(num1: number, num2: number) {
+//     return num1 + num2
+// }
+// const number1 = 5
+// const number2 = 2.3
+// const result = add(number1, number2)
+var person = {
+    name: 'Alex',
+    age: 35,
+    hobbies: ['Guitar', 'Tennis']
+};
+var favouriteActivities;
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.3;
-var result = add(number1, number2);
-console.log(result);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
-function add(num1: number, num2: number) {
-    return num1 + num2
+// function add(num1: number, num2: number) {
+//     return num1 + num2
+// }
+
+// const number1 = 5
+// const number2 = 2.3
+
+// const result = add(number1, number2)
+
+const person = {
+    name: 'Alex',
+    age: 35,
+    hobbies: ['Guitar', 'Tennis']
 }
+let favouriteActivities: string[]
 
-const number1 = 5
-const number2 = 2.3
+console.log(person.name)
 
-const result = add(number1, number2)
-
-console.log(result)
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
